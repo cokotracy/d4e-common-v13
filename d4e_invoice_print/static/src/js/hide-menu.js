ff5deb93accf842f6dc99486e7d108242d9fea71
iheb-bennouri <iheb.bennouri@campion-tech.com>
2021-06-18 11:01:46 +0100

    Update module d4e_invoice_print

--- d4e_invoice_print/static/src/js/hide-menu.js
+++ d4e_invoice_print/static/src/js/hide-menu.js
@@ -3,35 +3,61 @@ odoo.define('d4e_invoice_print.sidebar', function (require) {
 
 var core = require("web.core");
 var Sidebar = require("web.Sidebar");
-var rpc = require('web.rpc');
-var ajax = require('web.ajax');
-var _t = core._t;
 
 Sidebar.include({
     currentRecords: [],
-    readRecord: function (model, activeIds) {
+    _addRecords: function (model, resID) {
         var self = this;
-        return rpc.query({
+        if (!resID) {
+            return Promise.resolve();
+        }
+        return self._rpc({
+            context: self.env.context,
             model: model,
             method: 'search_read',
-            args: [[['id', 'in', activeIds]]],
-        }).then(function(rec) {
-            self.currentRecords = rec;
-            return rec;
+            args: [[['id', 'in', resID]]],
+        }).then(function (r) {
+            self.currentRecords = r;
         });
     },
-    start: function () {
-        return Promise.resolve(this.readRecord(this.env.model, this.env.activeIds)).then(this._super.bind(this));
-    },
-    rejectUnwantedSideMenu: function (sectionCode, items) {
-        var self = this;
-        if (items && sectionCode === 'other' && self.env && self.env.model === 'account.move' && self.env.context && self.env.context.default_type === 'in_invoice') {
-            items = _.reject(items, {label: _t("Print invoice with QR")});
+    _showItem: function (item) {
+        var tab = [];
+        if (item.classname) {
+            tab = item.classname.split(' ');
         }
-        return items;
+        tab.splice(tab.indexOf('d-none'), 1);
+        item.classname = tab.join(' ');
     },
-    _addItems: function (sectionCode, items) {
-        return this._super(sectionCode, this.rejectUnwantedSideMenu(sectionCode, items));
+    _hideItem: function (item) {
+        var tab = [];
+        if (item.classname) {
+            tab = item.classname.split(' ');
+        }
+        tab.push('d-none');
+        item.classname = tab.join(' ');
+    },
+    _condition: function (record, item) {
+        return this.env.model === 'account.move' && ((record && (record.state === 'draft' || record.type === 'in_invoice')) || this.env.context.default_type === 'in_invoice') && item.label === "Imprimer avec la facture QR";
+    },
+    _manageItemBefore: function () {
+        var self = this;
+        for (var i = 0; i < self.items.other.length; i++) {
+            var item = self.items.other[i], rec = null;
+            if (!_.isEmpty(self.currentRecords) && self.currentRecords.length === 1) {
+                var rec = self.currentRecords[0];
+            }
+            if (self._condition(rec, item)) {
+                self._hideItem(item);
+            } else {
+                self._showItem(item);
+            }
+        }
+    },
+    _redraw: function () {
+        var self = this, def = self._addRecords(self.env.model, self.env.activeIds).then(function () {
+            self._manageItemBefore();
+        });
+        return Promise.resolve(def).then(self._super.bind(self));
     },
 });
 
